10a40d32f49249b98bfb63d9df352e8184312050
LuiggySilva <luiggy.silva@ccc.ufcg.edu.br>
2019-10-21 14:20:20 -0300

    primeira parte do lab01 terminada

--- labs-frontend/lab01/scoord.js
+++ labs-frontend/lab01/scoord.js
@@ -47,7 +47,7 @@ function professor (matricula, nome, email, cpf, fotoURL) {
 			var retorno = [];
 			_turmas.forEach((t) => {
 				if (t.get_periodo() === semestre) {
-					retorno.push(t);
+					retorno.push(t.get_disciplina_ref());
 				}
 			});
 			return retorno;
@@ -102,7 +102,7 @@ function turma (disciplina_ref, perido) {
 	let _alunos = [];
 	let _disciplina_ref = disciplina_ref;
 	return {
-		get_professor : () => _professor,
+		get_professor : () => _professor.get_matricula(),
 		get_status : () => _status,
 		get_periodo : () => _periodo,
 		get_alunos : () => {
@@ -114,13 +114,20 @@ function turma (disciplina_ref, perido) {
 		},
 		get_disciplina_ref : () => _disciplina_ref,
 
-		set_professor : function (nome) {
-			_professor = nome;
+		set_professor : function (p) {
+			_professor = p;
 		},
 		matricular : function (aluno) {
-			if (!_alunos.includes(aluno) && (_status === "planejada" || _status === "ativa")){
+			let existe = false;
+			_alunos.forEach((a) => {
+				if (aluno.get_matricula() === a.get_matricula()){
+				    existe = true;
+				}	
+			})
+			if(!existe && (_status === "planejada" || _status === "ativa")){
 				_alunos.push(aluno);
 			}
+			
 		},
 		set_status : function (novo) {
 			switch (novo) {
@@ -145,74 +152,3 @@ function turma (disciplina_ref, perido) {
 		}
 	};
 }
-// Testes: Estudante
-console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Testes: Estudante -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-var t2 = turma("LP2", "2");
-var e1 = estudante(123456, "individuo1", "indi1@ccc.ufcg.edu.br", "8.8.4.4", "faicebuk.com");
-console.log(e1);
-console.log("estudante-nome: " + e1.get_nome());
-console.log("estudante-email: " + e1.get_email());
-console.log("estudante-cpf: " + e1.get_cpf());
-console.log("estudante-fotoURL: " + e1.get_fotoURL());
-console.log("estudante-turmas:");
-console.log(e1.get_turmas());
-e1.set_nome("individuo10");
-console.log("set-estudante-nome: " + e1.get_nome());
-e1.matricula(t2);
-console.log("set-estudante-turmas:");
-console.log(e1.get_turmas());
-console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=- Fim Testes: Estudante -=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
-// Testes: Turma
-console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Testes: Turma -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-var e2 = estudante(123, "e2", "e2@ccc.ufcg.edu.br", "1.2.3-4", "google");
-var e3 = estudante(321, "e3", "e3@ccc.ufcg.edu.br", "12.22.23-24", "google");
-var e4 = estudante(213, "e4", "e4@ccc.ufcg.edu.br", "21.22.23-24", "google");
-
-var t1 = turma("PSOFT", "4");
-console.log(t1);
-
-t1.set_professor("Dalton");
-console.log("turma-professor: " + t1.get_professor());
-console.log("turma-status: " + t1.get_status());
-console.log("turma-perido: " + t1.get_periodo());
-console.log("turma-ref_disc:" + t1.get_disciplina_ref());
-console.log("turma-alunos:");
-console.table(t1.get_alunos());
-t1.set_professor("Dalton e Raquel");
-console.log("set-turma-professor: " + t1.get_professor());
-t1.set_status("ativa");
-t1.set_status("bagunça");
-console.log("set-turma-status: " + t1.get_status());
-t1.matricular(e1);
-t1.matricular(e2);
-t1.matricular(e3);
-t1.matricular(e4);
-console.log("set-turma-alunos:");
-console.table(t1.get_alunos());
-t1.desmatricular(e1);
-console.log("set-turma-alunos:");
-console.table(t1.get_alunos());
-console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=- Fim Testes: Turma -=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-
-// Testes: Professor
-console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=- Testes: Professor -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-var p1 = professor(123456789, "Jubiscreiton", "jubi@ccc.ufcg.edu.br", "123.456.789-10", "www.acre.com.br/jubi");
-console.log(p1);
-
-console.log("professor-matricula: " + p1.get_matricula());
-console.log("professor-nome: " + p1.get_nome());
-console.log("professor-email: " + p1.get_email());
-console.log("professor-cpf: " + p1.get_cpf());
-console.log("professor-fotoURL: " + p1.get_fotoURL());
-p1.set_nome("Jubiscreiton Alfonso");
-console.log("set-professor-nome: " + p1.get_nome());
-console.log("professor_turmas:");
-console.table(p1.get_turmas());
-p1.aloca_turma(t1);
-p1.aloca_turma(t2);
-console.log("professor-aloca_turmas:");
-console.log(p1.get_turmas());
-console.log("professor-turmas(4):");
-console.log(p1.turmas("4")[0].get_disciplina_ref());
-console.log("=-=-=-=-=-=-=-=-=-=-=-=-=-=- Fim Testes: Estudante -=-=-=-=-=-=-=-=-=-=-=-=-=-=");
--- labs-frontend/lab01/test.js
+++ labs-frontend/lab01/test.js
@@ -5,7 +5,7 @@ let professor = require('./scoord').professor;
 let estudante = require('./scoord').estudante;
 
 describe('factory Disciplina', function() {
-  let d0;
+    let d0;
 
     before(async () => {
         d0 = disciplina('prog1', 'Programação 1', 4, []);
@@ -44,28 +44,144 @@ describe('factory Disciplina', function() {
 });
 
 describe('factory Professor', function() {
-  let p0;
+    let p0;
+    let t1;
+    let t2;
 
     before(async () => {
-        p0 = ;
+        p0 = professor(123456789, "Jubiscreiton", "jubi@ccc.ufcg.edu.br", "123.456.789-10", "www.acre.com.br/jubi");
+        t1 = turma("PSOFT", "4");
+        t2 = turma("FMCC2", "2");
     })
+
+    it('deve criar professores distintos a cada invocação', function(){
+        p0 = professor(123456789, "Jubiscreiton", "jubi@ccc.ufcg.edu.br", "123.456.789-10", "www.acre.com.br/jubi");
+        p1 = professor(123456789, "Jubiscreiton", "jubi@ccc.ufcg.edu.br", "123.456.789-10", "www.acre.com.br/jubi");
+        p2 = professor(123456789, "Jubiscreiton", "jubi@ccc.ufcg.edu.br", "123.456.789-10", "www.acre.com.br/jubi");
+        assert.notEqual(p0, p1);
+        assert.notEqual(p0, p2);
+        assert.notEqual(p1, p2);
+    });
+
+    it('deve reter os dados de inicialização', function(){
+        assert.equal(p0.get_matricula() , 123456789);
+        assert.equal(p0.get_nome() , "Jubiscreiton");
+        assert.equal(p0.get_email() , "jubi@ccc.ufcg.edu.br");
+        assert.equal(p0.get_cpf() , "123.456.789-10");
+        assert.equal(p0.get_fotoURL() ,"www.acre.com.br/jubi");
+        assert.deepEqual(p0.get_turmas(), []);
+    });
+
+    it('deve permitir atualização do nome', function(){
+        p0.set_nome("Jubiscreiton Alfonso");
+        assert.equal(p0.get_nome(), "Jubiscreiton Alfonso");
+    });
+
+    it('deve permitir a alocacao de turmas', function(){
+        p0.aloca_turma(t1);
+        p0.aloca_turma(t2);
+        assert.deepEqual(p0.get_turmas(), ['PSOFT', 'FMCC2']); 
+    });
+
+    it('deve ser possivel recuperar as turmas por periodo', function(){
+        assert.deepEqual(p0.turmas("4"), ['PSOFT']); 
+    });
 
 });
 
 describe('factory Estudante', function() {
-  let e0;
+    let e0;
+    let t1;
+    let t2;
 
     before(async () => {
-        e0 = ;
+        e0 = estudante(123456, "individuo1", "indi1@ccc.ufcg.edu.br", "1.2.3-4", "google.com");
+        t1 = turma("PSOFT", "4");
+        t2 = turma("FMCC2", "2");
     })
+
+    it('deve criar estudantes distintos a cada invocação', function(){
+        e0 = estudante(123456, "individuo1", "indi1@ccc.ufcg.edu.br", "1.2.3-4", "google.com");
+        e1 = estudante(123456, "individuo1", "indi1@ccc.ufcg.edu.br", "1.2.3-4", "google.com");
+        e2 = estudante(123456, "individuo1", "indi1@ccc.ufcg.edu.br", "1.2.3-4", "google.com");
+        assert.notEqual(e0, e1);
+        assert.notEqual(e0, e2);
+        assert.notEqual(e1, e2);
+    });
+
+    it('deve reter os dados de inicialização', function(){
+        assert.equal(e0.get_nome(), "individuo1");
+        assert.equal(e0.get_email(), "indi1@ccc.ufcg.edu.br");
+        assert.equal(e0.get_matricula(), 123456);
+        assert.equal(e0.get_cpf(), "1.2.3-4");
+        assert.equal(e0.get_fotoURL(), "google.com");
+        assert.deepEqual(e0.get_turmas(), []);
+    });
+
+    it('deve permitir atualização do nome', function(){
+        e0.set_nome("individuo10");
+        assert.equal(e0.get_nome(), "individuo10");
+    });
+
+    it('deve permitir a matricula em turmas', function(){
+        assert.deepEqual(e0.get_turmas(), []);
+        e0.matricula(t1);
+        e0.matricula(t2);
+        assert.deepEqual(e0.get_turmas(), ['PSOFT', 'FMCC2']);
+    });
 
 });
 
 describe('factory Turma', function() {
   let t0;
+  let p0;
+  let p1;
+  let e0;
 
     before(async () => {
-        t0 = ;
+        t0 = turma("PSOFT", "4");
+        p0 = professor(123456789, "Jubiscreiton", "jubi@ccc.ufcg.edu.br", "123.456.789-10", "www.acre.com.br/jubi");
+        p1 = professor(987654321, "Josicreiton", "josi@ccc.ufcg.edu.br", "923.456.789-10", "www.acre.com.br/jubi");
+        e0 = estudante(123456, "individuo1", "indi1@ccc.ufcg.edu.br", "1.2.3-4", "google.com");
+        
     })
+
+    it('deve criar estudantes distintos a cada invocação', function(){
+        t0 = turma("PSOFT", "4");
+        t1 = turma("PSOFT", "4");
+        t2 = turma("PSOFT", "4");
+        assert.notEqual(t0, t1);
+        assert.notEqual(t0, t2);
+        assert.notEqual(t1, t2);
+    });
+
+    it('deve reter os dados de inicialização', function(){
+        t0.set_professor(p0);
+        assert.equal(t0.get_professor(), 123456789);
+        assert.equal(t0.get_status(), "planejada");
+        assert.equal(t0.get_periodo(), "4");
+        assert.equal(t0.get_disciplina_ref(), "PSOFT");
+        assert.deepEqual(t0.get_alunos(), []);
+    });
+
+    it('deve permitir atualização do professor e status', function(){
+        t0.set_professor(p1);
+        assert.equal(t0.get_professor(), 987654321);
+        t0.set_status("ativa");
+        assert.equal(t0.get_status(), "ativa");
+        t0.set_status("concluída");
+        assert.equal(t0.get_status(), "concluída");
+        t0.set_status("rasgado");
+        assert.equal(t0.get_status(), "concluída");
+    });
+
+    it('deve ser possivel matricular alunos', function(){
+        let t0 = turma("PSOFT", "4");
+        t0.matricular(e0);
+        t0.matricular(e0);
+        assert.deepEqual(t0.get_alunos(), [123456]);
+        t0.desmatricular(e0);
+        assert.deepEqual(t0.get_alunos(), []);        
+    });
 
 });
